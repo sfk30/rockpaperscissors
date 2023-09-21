322cfc47f1e0dc95574982792bf328669d8f610f
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-21 20:44:55 +0100

    Completed rock, paper, scissors

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 let arr = ['rock', 'paper', 'scissors']
 
 function getComputerChoice() {
-    console.log(arr[Math.floor(Math.random() * arr.length)]);
+    return (arr[Math.floor(Math.random() * arr.length)]);
 }
 
 let computerSelection = getComputerChoice();
 
-let youChoose = prompt('Pick rock, paper or scissors?', '');
-let playerSelection = youChoose.toLowerCase();
+console.log(computerSelection);
 
-if (computerSelection === playerSelection) { 
-    console.log('It\'s a draw!');
-}   else if (computerSelection === 'rock' && playerSelection === 'paper') {
-    console.log('You lose! Rock beats paper!')
-}   else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-    console.log('You lose! Scissors beats paper!')
-}   else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-    console.log('You win! Scissors beats paper!')
-}   else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-    console.log('You lose! Rock beats scissors!')
-}   else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-    console.log('You win! Rock beats scissors!')
-}   else if (computerSelection === 'paper' && playerSelection === 'rock') {
-    console.log('You win! Rock beats paper!')
-}   else {
-    console.log('Error please try again')
+function yourChoice() {
+     return prompt('Pick rock, paper or scissors?').toLowerCase();
 }
+
+let playerSelection = yourChoice();
+
+console.log(playerSelection);
+
+    function roundOne(computerSelection, playerSelection) {
+        if (computerSelection === playerSelection) { 
+            console.log('It\'s a draw!');
+        }   else if (computerSelection === 'rock' && playerSelection === 'paper') {
+            console.log('You lose! Rock beats paper!')
+        }   else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+            console.log('You lose! Scissors beats paper!')
+        }   else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+            console.log('You win! Scissors beats paper!')
+        }   else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+            console.log('You lose! Rock beats scissors!')
+        }   else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+            console.log('You win! Rock beats scissors!')
+        }   else if (computerSelection === 'paper' && playerSelection === 'rock') {
+            console.log('You win! Rock beats paper!')
+        }   else {
+            console.log('Error please try again')
+        }
+    }
+
+roundOne(computerSelection, playerSelection); 
+
+// if you run just roundOne() with no arguments you get draw always as computerSelection and 
+// player selection are undefined so computerSelection === playerSelection is always true as 
+// undefined ==== undefined
