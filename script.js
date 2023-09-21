e4faa1e3ad9288066b32a3c09b719443dff8dbc1
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-21 17:48:38 +0100

    Added if, else if, and else statements

--- script.js
+++ script.js
@@ -4,4 +4,25 @@ function getComputerChoice() {
     console.log(arr[Math.floor(Math.random() * arr.length)]);
 }
 
-getComputerChoice();
+let computerSelection = getComputerChoice();
+
+let youChoose = prompt('Pick rock, paper or scissors?', '');
+let playerSelection = youChoose.toLowerCase();
+
+if (computerSelection === playerSelection) { 
+    console.log('It\'s a draw!');
+}   else if (computerSelection === 'rock' && playerSelection === 'paper') {
+    console.log('You lose! Rock beats paper!')
+}   else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+    console.log('You lose! Scissors beats paper!')
+}   else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+    console.log('You win! Scissors beats paper!')
+}   else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+    console.log('You lose! Rock beats scissors!')
+}   else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+    console.log('You win! Rock beats scissors!')
+}   else if (computerSelection === 'paper' && playerSelection === 'rock') {
+    console.log('You win! Rock beats paper!')
+}   else {
+    console.log('Error please try again')
+}
