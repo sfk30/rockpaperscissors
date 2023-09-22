f2fef8266794527dd7607ebe56a5c95440e895f3
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-22 11:00:51 +0100

    Added in loop for 5 rounds but prompts for 6

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice();
 
-console.log(computerSelection);
+// console.log(computerSelection);
 
 function yourChoice() {
      return prompt('Pick rock, paper or scissors?').toLowerCase();
@@ -14,30 +14,64 @@ function yourChoice() {
 
 let playerSelection = yourChoice();
 
-console.log(playerSelection);
+// console.log(playerSelection);
 
-    function roundOne(computerSelection, playerSelection) {
+let computerScore = 0;
+let playerScore = 0;
+
+    function playRound(computerSelection, playerSelection) {
         if (computerSelection === playerSelection) { 
             console.log('It\'s a draw!');
         }   else if (computerSelection === 'rock' && playerSelection === 'paper') {
             console.log('You lose! Rock beats paper!')
+            computerScore ++;
         }   else if (computerSelection === 'scissors' && playerSelection === 'paper') {
             console.log('You lose! Scissors beats paper!')
+            computerScore ++;
         }   else if (computerSelection === 'paper' && playerSelection === 'scissors') {
             console.log('You win! Scissors beats paper!')
+            playerScore ++;
         }   else if (computerSelection === 'rock' && playerSelection === 'scissors') {
             console.log('You lose! Rock beats scissors!')
+            computerScore ++;
         }   else if (computerSelection === 'scissors' && playerSelection === 'rock') {
             console.log('You win! Rock beats scissors!')
+            playerScore ++;
         }   else if (computerSelection === 'paper' && playerSelection === 'rock') {
             console.log('You win! Rock beats paper!')
+            playerScore ++;
         }   else {
             console.log('Error please try again')
         }
     }
 
-roundOne(computerSelection, playerSelection); 
+// playRound(computerSelection, playerSelection);
+// playRound(computerSelection, playerSelection); 
 
 // if you run just roundOne() with no arguments you get draw always as computerSelection and 
 // player selection are undefined so computerSelection === playerSelection is always true as 
 // undefined ==== undefined
+
+
+function game() {
+    for (i = 1; i <= 5; i ++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = yourChoice();
+        playRound(computerSelection, playerSelection)
+    }   
+    if (playerScore > computerScore) {
+        console.log(`player score = ${playerScore}, computer score = ${computerScore}, Player wins!`)
+    }   else {
+        console.log(`player score = ${playerScore}, computer score = ${computerScore}, Computer wins!`)
+    }
+}
+
+game();
+
+
+
+// if (counter >= 3) {
+//     console.log('You win!')
+// }   else {
+//     console.log('You lose!')
+// }
